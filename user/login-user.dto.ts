a8d0c440e0143668f7158652e37025ca50bd05c3
YAziX _01 <101416348+YeameenAziz@users.noreply.github.com>
2025-08-22 09:38:13 +0600

    Delete user directory

--- user/login-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsString } from 'class-validator';
-
-export class LoginUserDto {
-  @IsEmail({}, { message: 'Email format is not correct' })
-  email: string;
-
-  @IsString({ message: 'Password must be a string' })
-  password: string;
-}
