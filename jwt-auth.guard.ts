b1e767e5cafca0fae0890344e49f1412816de418
YAziX _01 <101416348+YeameenAziz@users.noreply.github.com>
2025-08-22 09:47:16 +0600

    Delete jwt-auth.guard.ts

--- jwt-auth.guard.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-/**
- * Custom JWT Authentication Guard
- * This guard ensures that routes are protected and require a valid JWT token.
- */
-@Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
